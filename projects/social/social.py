ad14768f8dfff3d9a3ab3c400d7f250e1d982507
biskoi <biskoi@daydream.cafe>
2020-08-12 14:41:33 -0600

    day3 mvp

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,10 @@
+import random
+from util import Queue
+
 class User:
-    def __init__(self, name):
+    def __init__(self, name, id):
         self.name = name
+        self.id = id
 
 class SocialGraph:
     def __init__(self):
@@ -25,7 +29,7 @@ class SocialGraph:
         Create a new user with a sequential integer ID
         """
         self.last_id += 1  # automatically increment the ID to assign the new user
-        self.users[self.last_id] = User(name)
+        self.users[self.last_id] = User(name, self.last_id)
         self.friendships[self.last_id] = set()
 
     def populate_graph(self, num_users, avg_friendships):
@@ -45,8 +49,26 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(i)
 
         # Create friendships
+
+        possible = []
+
+        #add combinations to possible list
+        for user in self.users:
+            for i in range(user + 1, self.last_id + 1):
+                if user == i:
+                    continue
+
+                possible.append([user, i])
+
+        n = avg_friendships * self.last_id # for each user, plan to give them this many friends
+        random.shuffle(possible) # randomizes so it's no longer in order, otherwise the first users would get all the friends
+        # print(possible)
+        for i in range(0, n // 2): # each op will make two people friends with each other, so halve the number
+            self.add_friendship(possible[i][0], possible[i][1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,12 +81,43 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
-        return visited
+        #bfs
+        q = Queue()
+        q.enqueue([user_id]) #enqueue starting node
+
+        while q.size() > 0:
+            curr_path = q.dequeue()
+            node = curr_path[-1]
+
+            if len(curr_path) > 1:
+                
+                if node in visited or node == user_id:
+                    continue
+                elif node not in visited:
+                    visited[node] = curr_path
+                elif len(visited[node]) > len(curr_path):
+                    visited[node] = curr_path
+            
+            # print(self.users[node].id)
+
+            for item in self.friendships[node]:
+                path_copy = curr_path.copy()
+                path_copy.append(item)
+                q.enqueue(path_copy)
+
+        avg_separation = 0
+
+        for node in visited:
+            avg_separation += len(visited[node])
+
+        pop_percent = len(visited) / self.last_id
+        
+        return [pop_percent, avg_separation // len(visited), visited]
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
+    sg.populate_graph(1000, 5)
+    # print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
